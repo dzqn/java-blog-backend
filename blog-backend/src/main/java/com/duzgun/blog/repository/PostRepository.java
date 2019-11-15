79cbbf59301d8963cabd274919d609e9192a19bc
Düzgün Tutar <duzguntutar@gmail.com>
2019-11-15 14:51:47 +0300

    Update PostRepository.java

--- blog-backend/src/main/java/com/duzgun/blog/repository/PostRepository.java
+++ blog-backend/src/main/java/com/duzgun/blog/repository/PostRepository.java
@@ -1,9 +1,7 @@
 package com.duzgun.blog.repository;
 
 import org.springframework.data.repository.CrudRepository;
-
 import com.duzgun.blog.model.entity.Post;
 
 public interface PostRepository extends CrudRepository<Post,Long> {
-
 }
