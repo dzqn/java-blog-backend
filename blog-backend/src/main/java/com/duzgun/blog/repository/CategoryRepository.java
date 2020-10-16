6cbd7abb16454f221ab3295febfebd6c67c11a34
Düzgün Tutar <duzguntutar@gmail.com>
2020-10-17 00:17:04 +0300

    Update CategoryRepository.java

--- blog-backend/src/main/java/com/duzgun/blog/repository/CategoryRepository.java
+++ blog-backend/src/main/java/com/duzgun/blog/repository/CategoryRepository.java
@@ -1,7 +1,7 @@
 package com.duzgun.blog.repository;
 
 import org.springframework.data.repository.CrudRepository;
-import com.duzgun.blog.model.entity.Category;;
+import com.duzgun.blog.model.entity.Category;
 
 public interface CategoryRepository extends CrudRepository<Category, Long> {
 }
